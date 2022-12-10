dafd421ff084b9e3bb1f546ac39a2a69bcd75175
Jobayer Al Madmud Tuser <jtuser112@gmail.com>
2022-12-10 09:59:40 +0600

    some changes in idnex file

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 declare(strict_types=1);
 
 require (__DIR__ .'/../vendor/autoload.php');
-define("VIEW_PATH", __DIR__ . "/../views");
+define("VIEW_PATH", __DIR__ . "/../resources");
 define("CACHE_PATH", __DIR__ . "/../cache");
 
 //require (__DIR__ . "/../routes/route.php");
